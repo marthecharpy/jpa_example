5e5b7dabc461cdf3ee70af372cc51e557683a6b7
marthe.charpy <marthe.charpy@gmail.com>
2019-01-17 18:00:09 +0100

    Tag.java and Post.java created

--- src/main/java/com/example/jpa/jpamanytomanydemo/entities/Tag.java
+++ src/main/java/com/example/jpa/jpamanytomanydemo/entities/Tag.java
@@ -1,31 +1,37 @@
 package com.example.jpa.jpamanytomanydemo.entities;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.*;
-
 @Entity
-public class Post {
+public class Tag {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Post() {
-
-    }
     @ManyToMany(fetch = FetchType.LAZY,
             cascade = {
                 CascadeType.PERSIST,
                 CascadeType.MERGE
-            })
-    @JoinTable(name = "post_tags",
-            joinColumns = { @JoinColumn(name = "post_id") },
-            inverseJoinColumns = { @JoinColumn(name = "tag_id") })
-    private Set<Tag> tags = new HashSet<>();
-    
+            },
+            mappedBy = "tags")
+    private Set<Post> posts = new HashSet<>();
+
+    public Tag() {
+
+    }
+
 	public Long getId() {
 		return id;
+	}
+
+	public Set<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(Set<Post> posts) {
+		this.posts = posts;
 	}
 
 }
